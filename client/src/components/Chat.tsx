ad5c8835a9618202f3d589129a832dbb899f292b
reynaldomarchell <reynaldomarchellba@gmail.com>
2024-03-28 16:00:48 +0700

    fix: change api endpoint to deployed vercel

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -32,7 +32,10 @@ export default function Chat() {
       };
 
       setIsLoading(true);
-      const response = await fetch("http://localhost:8000/aoba", options);
+      const response = await fetch(
+        "https://chat-aoba-server.vercel.app/aoba",
+        options,
+      );
       const data = await response.json();
 
       setChatHistory((previousChat) => [
