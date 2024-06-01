7edbf8ba418079b0888bfe41e542839dc1e7db2f
reynaldomarchell <reynaldomarchellba@gmail.com>
2024-06-01 14:52:00 +0700

    perf: change model to gemini-1.5-flash-latest

--- server/index.ts
+++ server/index.ts
@@ -47,7 +47,7 @@ app.post("/aoba", async (req, res) => {
     ];
 
     const model = genAI.getGenerativeModel({
-      model: "gemini-1.5-pro-latest",
+      model: "gemini-1.5-flash-latest",
       safetySettings,
     });
 
